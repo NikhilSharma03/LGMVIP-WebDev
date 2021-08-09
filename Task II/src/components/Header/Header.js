4d809a7815d16a26bee6ac030ba8f17dd0c60291
Nikhil Sharma <nikhilsharmamusic2000@gmail.com>
2021-08-09 17:31:15 +0530

    Initialized project ii

---
+++ Task II/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>FetchUser</h1>
+      <button>Get Users</button>
+    </header>
+  );
+}
+
+export default Header;
